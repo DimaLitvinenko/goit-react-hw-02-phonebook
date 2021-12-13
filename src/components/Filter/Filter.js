372a6c2ac81f6607590abf12c38ac91c50007107
DimaLitvinenko <dimaly.too@gmail.com>
2021-12-13 14:23:06 +0200

    upgrade

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,20 +3,25 @@ import PropTypes from 'prop-types';
 import style from './Filter.module.scss';
 
 export default function Filter({ contactName, findByNameHandler }) {
-	return (
-		<label className={style.filter__label}>
-			Find contacts by name
-			<input
-				className={style.filter__input}
-				type="text"
-				value={contactName}
-				onChange={findByNameHandler}
-			/>
-		</label>
-	);
+   return (
+      <div className={style.filter__container}>
+         <input
+            className={style.filter__input}
+            id="input"
+            type="text"
+            value={contactName}
+            onChange={findByNameHandler}
+            placeholder=" "
+         />
+         <div className={style.cut}></div>
+         <label className={style.filter__label} htmlFor="input">
+            Find contacts by name
+         </label>
+      </div>
+   );
 }
 
 Filter.propTypes = {
-	contactName: PropTypes.string.isRequired,
-	findByNameHandler: PropTypes.func.isRequired,
+   contactName: PropTypes.string.isRequired,
+   findByNameHandler: PropTypes.func.isRequired,
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'modern-normalize/modern-normalize.css';
+import 'modern-normalize/modern-normalize.css';
 import './index.scss';
 import App from './App';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+   document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
